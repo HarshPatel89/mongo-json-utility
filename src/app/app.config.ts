97bceefd6b431459a74e829b41c968d73c4d629f
HarshPatel89 <harsh.b.patel@convergesolution.com>
2024-09-19 16:19:59 +0530

    added changes and models files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient( withFetch()),
+    provideAnimationsAsync(),
+    MessageService,
+
+  ]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    DropdownModule,
+    InputTextareaModule,
+    ButtonModule,
+
+
+
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  selectedEntity: string = '';
+  entities: any[] = [
+    {
+      id: 1,
+      name: 'Activity'
+    },
+    {
+      id: 2,
+      name: 'ActivityDetail'
+    },
+    {
+      id: 3,
+      name: 'Track'
+    },
+    // {
+    //   id: 4,
+    //   name:''
+    // },
+  ];
+  jsonToConvert: any;
+  convertedJson: any ;
 
+
+
+
+
+
+
+
+
+  constructor(
+
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  convertJson(){
+
+
+  }
+
+
+  
 }
